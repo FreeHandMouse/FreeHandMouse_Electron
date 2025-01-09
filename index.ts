2645105f0b665324fbfe77e96ac044598712d0e8
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-01-09 20:32:50 +0900

    どうにかしてカメラ画像を食わせねばならない

--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
 import { setCursorPos } from "./user32";
+import { FilesetResolver, HandLandmarker } from "@mediapipe/tasks-vision";
 
-setCursorPos(0, 0);
+const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+);
+
+const handLandmarker = await HandLandmarker.createFromOptions(
+    vision,
+    {
+        baseOptions: {
+            modelAssetPath: "hand_landmarker.task"
+        },
+        numHands: 2,
+    }
+);
+
+const handLandmarkerResult = handLandmarker.detect();
+
+    setCursorPos(0, 0);
