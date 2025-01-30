2ffcda5f6567ed37ba7755d0863ed1781909f4de
Yuki-Yui <yuukiyuinyan@gmail.com>
2025-01-31 00:39:09 +0900

    指の角度計算：二本のベクトル

--- electron/landmark-util.ts
+++ electron/landmark-util.ts
@@ -62,5 +62,14 @@ export function landmarkDot(landmark1: Landmark, landmark2: Landmark, landmark3:
 
 // TODO: 実装してね
 export function landmarkAngle(landmark1: Landmark, landmark2: Landmark, landmark3: Landmark) {
-  return Math.PI / 2;
+  const vector1 = [landmark2.x - landmark1.x, landmark2.y - landmark1.y, landmark2.z - landmark1.z];
+  const vector2 = [landmark3.x - landmark2.x, landmark3.y - landmark2.y, landmark3.z - landmark2.z];
+  const dot = vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector2[2];
+  const magnitude1 = Math.sqrt(vector1[0] ** 2 + vector1[1] ** 2 + vector1[2] ** 2);
+  const magnitude2 = Math.sqrt(vector2[0] ** 2 + vector2[1] ** 2 + vector2[2] ** 2);
+  if (magnitude1 === 0 || magnitude2 === 0) {
+    return Math.PI / 2;
+  }
+  const angle = Math.acos(dot / (magnitude1 * magnitude2));
+  return angle
 }
